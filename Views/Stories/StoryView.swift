02aacf45575d3c84267d0087f6dce1daefa5bf32
BrotherZemurro3 <konevichenko@icloud.com>
2025-08-06 11:27:24 +0300

    begin of work

---
+++ Views/Stories/StoryView.swift
@@ -0,0 +1,20 @@
+//
+//  StoryView.swift
+//  Travel Schedule
+//
+//  Created by Дионисий Коневиченко on 06.08.2025.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    StoryView()
+}
