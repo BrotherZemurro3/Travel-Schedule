b471b4e1b586a1757768e96ee8c3daa6eca17f32
BrotherZemurro3 <konevichenko@icloud.com>
2025-08-06 11:51:57 +0300

    added hash(into:)

--- Views/Stories/Stories.swift
+++ Views/Stories/Stories.swift
@@ -12,4 +12,16 @@ struct Stories: Identifiable, Hashable {
     var id = UUID()
     var previewImage: String
     var images: [String]
+    
+    
+    // Хэширование для Hashable
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        
+    }
+    
+    static func ==(lhs: Stories, rhs: Stories) -> Bool {
+        lhs.id == rhs.id
+    }
 }
