b0842d4dd489105384642d3e13e2489688af6500
BrotherZemurro3 <konevichenko@icloud.com>
2025-08-06 12:04:25 +0300

    updated StoriesViewModel, added currentStory && currentImageIndex

--- Views/Stories/StoriesViewModel.swift
+++ Views/Stories/StoriesViewModel.swift
@@ -8,23 +8,36 @@
 import Foundation
 import SwiftUI
 import Observation
+import Combine
 
 
 @Observable class StoriesViewModel {
     var story: [Stories]
     var showStoryView: Bool = false
+    var currentStoryIndex: Int = 0
+    var currentImageIndex: Int = 0
     
+    private var timer: Timer.TimerPublisher = Timer.publish(every: 10, on: .main, in: .common)
+    private var cancellable: AnyCancellable?
     init() {
             self.story = [
-                Stories(previewImage: "TwoPassengersPreview", BigImage: "TwoPassengersBig"),
-                Stories(previewImage: "TrainMountainPreview", BigImage: "TrainMountainBig"),
-                Stories(previewImage: "TrainFloversPreview", BigImage: "TrainFloversBig"),
-                Stories(previewImage: "PassengersPreview", BigImage: "PassengersBig"),
-                Stories(previewImage: "ManWithAccordionPreview", BigImage: "ManWithAccordionBig"),
-                Stories(previewImage: "MachineWorkerPreview", BigImage: "MachineWorkerBig"),
-                Stories(previewImage: "GrannyWithVegetablesPreview", BigImage: "GrannyWithVegetablesBig"),
-                Stories(previewImage: "FreeSpacePreview", BigImage: "FreeSpaceBig"),
-                Stories(previewImage: "ConductorGirlPreview", BigImage: "ConductorGirlBig")
+                Stories(previewImage: "TwoPassengersPreview", images: ["TwoPassengersBig","musiciansBig"]),
+                Stories(previewImage: "TrainMountainPreview", images: ["TrainMountainBig","winterTrainBig"]),
+                Stories(previewImage: "TrainFloversPreview", images: ["TrainFloversBig", "indianTrainBig"]),
+                Stories(previewImage: "PassengersPreview", images: ["PassengersBig", "leaderBig"]),
+                Stories(previewImage: "ManWithAccordionPreview", images: ["ManWithAccordionBig", "musiciansBig"]),
+                Stories(previewImage: "MachineWorkerPreview", images: ["MachineWorkerBig", "stationWorkerBig"]),
+                Stories(previewImage: "GrannyWithVegetablesPreview", images: ["GrannyWithVegetablesBig", "pumpkinTrainBig"]),
+                Stories(previewImage: "FreeSpacePreview", images: ["FreeSpaceBig", "ambientBig"]),
+                Stories(previewImage: "ConductorGirlPreview", images: ["ConductorGirlBig", "ConductorGirlTwoBig"])
             ]
         }
+    
+    func startTimer() {
+        timer = Timer.publish(every: 10, on: .main, in: .common)
+        cancellable = timer
+            .autoconnect()
+            .sink {}
+        
+    }
 }
