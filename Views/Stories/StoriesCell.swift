593df753087e7c0d41d23525f7be91c3d1f3ccdf
BrotherZemurro3 <konevichenko@icloud.com>
2025-08-07 18:00:33 +0300

    added text to storyCell on preview

--- Views/Stories/StoriesCell.swift
+++ Views/Stories/StoriesCell.swift
@@ -13,17 +13,33 @@ struct StoriesCell: View {
     let imageWidth: Double = 92
     @EnvironmentObject var viewModel: StoriesViewModel
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(stories.previewImage)
-                .resizable()
-                .cornerRadius(16)
-                .frame(width: imageWidth, height: imageHeight)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(Color.blue, lineWidth: 4)
-                )
-        }
-        .padding(.horizontal, 4)
+        ZStack(alignment: .leading){
+            
+            VStack(alignment: .leading) {
+                Image(stories.previewImage)
+                    .resizable()
+                    .cornerRadius(16)
+                    .frame(width: imageWidth, height: imageHeight)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.blue, lineWidth: 4)
+                    )
+                
+            }
+            VStack {
+                Text("Text Text Text Text Text Text Text")
+                    .font(.system(size: 12, weight: .regular))
+                    .frame(width: 76, height: 45)
+                    .foregroundStyle(.white)
+                    .lineLimit(3)
+                    
+                
+            }
+            .padding(.init(top: 83, leading: 0, bottom: 12, trailing: 8))
+            
+            .padding(.horizontal, 4)
+          
+            }
         .onTapGesture {
             if let index = viewModel.story.firstIndex(where: { $0.id == stories.id }) {
                 viewModel.selectStory(at: index)
