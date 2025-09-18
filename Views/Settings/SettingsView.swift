4efef05190774e44832f6900a688b9ef384b98f5
BrotherZemurro3 <konevichenko@icloud.com>
2025-09-18 17:42:29 +0300

    settings to MVVM

--- Views/Settings/SettingsView.swift
+++ Views/Settings/SettingsView.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @AppStorage("theme") private var selectedTheme: Theme = .auto
+    @Environment(SettingsViewModel.self) private var settingsViewModel
     @Environment(\.colorScheme) private var colorScheme
-    @State private var isDarkMode: Bool = false
-    @State private var navigationPath = NavigationPath()
+
 
     var body: some View {
-        NavigationStack(path: $navigationPath) {
+        @Bindable var settingsViewModel = settingsViewModel
+        NavigationStack(path: $settingsViewModel.navigationPath) {
             VStack(alignment: .leading, spacing: 16) {
                 HStack {
                     Text("Тёмная тема")
                         .font(.system(size: 17, weight: .regular))
                         .foregroundStyle(.blackDay)
                     Spacer()
-                    Toggle("", isOn: $isDarkMode)
+                    Toggle("", isOn: $settingsViewModel.isDarkMode)
                         .labelsHidden()
                 }
                 .padding(.horizontal, 16)
@@ -39,7 +39,7 @@ struct SettingsView: View {
                    
                     Spacer()
                     Button(action: {
-                         navigationPath.append(SettingsDestination.agreement)
+                        settingsViewModel.navigationPath.append(SettingsDestination.agreement)
                     }) { Image(systemName: "chevron.forward")
                             .foregroundStyle(.blackDay)
                     }
@@ -51,7 +51,7 @@ struct SettingsView: View {
                 }
                 
                 Button(action: {
-                    navigationPath.append(SettingsDestination.noInternet)
+                    settingsViewModel.navigationPath.append(SettingsDestination.noInternet)
                 }) {
                     Text("Показать экран 'Нет интернета'")
                         .font(.system(size: 17, weight: .regular))
@@ -64,7 +64,7 @@ struct SettingsView: View {
                 }
                 
                 Button(action: {
-                    navigationPath.append(SettingsDestination.serverError)
+                    settingsViewModel.navigationPath.append(SettingsDestination.serverError)
                 }) {
                     Text("Показать экран 'Ошибка сервера'")
                         .font(.system(size: 17, weight: .regular))
@@ -92,33 +92,28 @@ struct SettingsView: View {
                 .padding(.horizontal, 30)
                 }
             .toolbar(.visible, for: .tabBar)
-            .preferredColorScheme(selectedTheme == .auto ? nil : (selectedTheme == .dark ? .dark : .light))
+            .preferredColorScheme(settingsViewModel.colorScheme)
             .onAppear {
-                // Устанавливаю начальное состояние Toggle
-                isDarkMode = selectedTheme == .dark || (selectedTheme == .auto && colorScheme == .dark)
+                settingsViewModel.isDarkMode = settingsViewModel.selectedTheme == .dark ||
+                    (settingsViewModel.selectedTheme == .auto && colorScheme == .dark)
             }
-            .onChange(of: isDarkMode) { newValue in
-                // Обновляю selectedTheme при изменении Toggle
-                selectedTheme = newValue ? .dark : .light
+            .onChange(of: settingsViewModel.isDarkMode) { newValue in
+                settingsViewModel.updateDarkMode(newValue)
             }
             .onChange(of: colorScheme) { newValue in
-                // Обновляю isDarkMode при изменении системной темы, если selectedTheme == .auto
-                if selectedTheme == .auto {
-                    isDarkMode = newValue == .dark
-                }
+                settingsViewModel.updateToggleForSystemTheme(newValue)
             }
             .navigationDestination(for: SettingsDestination.self) { destination in
                 Group {
                     switch destination {
                     case .noInternet:
-                        NoInternetView(navigationPath: $navigationPath)
+                        NoInternetView(navigationPath: $settingsViewModel.navigationPath)
                     case .serverError:
-                        ServerErrorView(navigationPath: $navigationPath)
+                        ServerErrorView(navigationPath: $settingsViewModel.navigationPath)
                     case .agreement:
-                        AgreementView(navigationPath: $navigationPath)
+                        AgreementView(navigationPath: $settingsViewModel.navigationPath)
 
-                    }
-                }
+                    }                }
             }
         }
     }
